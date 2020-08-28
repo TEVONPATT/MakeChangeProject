0c803ad37f604f066c3ba23c1148543f2a5faa22
Tevon Patterson <Tevonpatt@gmail.com>
2020-08-28 16:31:24 -0700

    added in scanner, item price, customer payment, and amount owed.

--- src/makechange/MakeChange.java
+++ src/makechange/MakeChange.java
@@ -1,7 +1,19 @@
 package makechange;
 
+import java.util.Scanner;
+
 public class MakeChange {
 	public static void main(String[] args) {
-
+		Scanner scan = new Scanner(System.in);
+		System.out.print("Enter in the item price: $");
+//		double money = scan.nextDouble();
+		double itemPrice = scan.nextDouble();
+		
+		System.out.print("Amount tendered by customer: $");
+		double customer = scan.nextDouble();
+	
+		double amtOwed = itemPrice - customer;
+		System.out.println("The remaining balance is " + amtOwed);
+		
 	}
 }
