354c05871930a4df750ed47aa8f05b730a335f9e
Tevon Patterson <Tevonpatt@gmail.com>
2020-08-29 16:28:44 -0700

    cash register working

--- src/makechange/MakeChange.java
+++ src/makechange/MakeChange.java
@@ -6,6 +6,7 @@ public class MakeChange {
 	public static void main(String[] args) {
 		int twentyDollar = 0;
 		int tenDollar = 0;
+		int fiveDollar = 0;
 		int oneDollar = 0;
 		int quarter = 0;
 		int dime = 0;
@@ -29,13 +30,54 @@ public class MakeChange {
 			System.out.println("Balance remaining is $00.00");
 		}
 		else {
-			while( change >= 20) {
+			while(change >= 20) {
 				twentyDollar++;
 				change -= 20;
 			}
-			System.out.println("Your change is $" + change);
+			while(change >= 10) {
+				tenDollar++;
+				change -= 10;
+			}
+			while(change >= 5) {
+				fiveDollar++;
+				change -= 5;
+			}
+			while(change >= 1) {
+				oneDollar++;
+				change -= 1;
+			}
+			while(change >= .25) {
+				quarter++;
+				change -= .25;
+			}
+			while(change >= .10) {
+				dime++;
+				change -= .10;
+			}
+			while(change >= .05) {
+				nickel++;
+				change -= .05;
+			}
+			while(change >= .01) {
+				penny++;
+				change -= .01;
+			}
+			System.out.printf("Your change is $" + change);
+			System.out.println();
 			System.out.println(twentyDollar + " twenty dollar bills");
-			
+			System.out.println(tenDollar + " ten dollar bills");
+			System.out.println(fiveDollar + " five dollar bills");
+			System.out.println(oneDollar + " one dollar bills");
+			System.out.println(quarter + " quarters");
+			System.out.println(dime + " dimes");
+			System.out.println(nickel + " nickels");
+			System.out.printf(penny + " pennies");
+//			if(change == .01) {
+//				System.out.println(penny + " penny");
+//			}
+//			else {
+//				System.out.printf(penny + " pennies");
+//			}
 		}
 //		double amtOwed = itemPrice - customer;
 //		double change = customer - itemPrice;
